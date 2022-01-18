e615f0183c8ceb34b978d216eb42a9db35c47875
Samaresh <samaresh.b.mail@gmail.com>
2022-01-18 15:02:55 +0530

    uploaded scheduled cron-cron job example module

---
+++ Schedule Cron Job/Schedule-Cron/Schedule-Cron/src/main/java/Schedule/Cron/constants/ScheduleCronPortletKeys.java
@@ -0,0 +1,11 @@
+package Schedule.Cron.constants;
+
+/**
+ * @author sbarui002
+ */
+public class ScheduleCronPortletKeys {
+
+	public static final String SCHEDULECRON =
+		"Schedule_Cron_ScheduleCronPortlet";
+
+}
---
+++ Schedule Cron Job/Schedule-Cron/Schedule-Cron/src/main/java/Schedule/Cron/portlet/ScheduleCronPortlet.java
@@ -0,0 +1,139 @@
+package Schedule.Cron.portlet;
+
+import com.liferay.mail.kernel.model.MailMessage;
+import com.liferay.mail.kernel.service.MailServiceUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.messaging.BaseMessageListener;
+import com.liferay.portal.kernel.messaging.DestinationNames;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.scheduler.SchedulerEngineHelperUtil;
+import com.liferay.portal.kernel.scheduler.SchedulerEntryImpl;
+import com.liferay.portal.kernel.scheduler.SchedulerException;
+import com.liferay.portal.kernel.scheduler.Trigger;
+import com.liferay.portal.kernel.scheduler.TriggerFactoryUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PropsUtil;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import javax.mail.internet.InternetAddress;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jwc.insta.model.HandleAPIKeys;
+import jwc.insta.service.HandleAPIKeysLocalServiceUtil;
+
+/*import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;*/
+
+/**
+ * @author sbarui002
+ */
+@Component(
+	immediate = true,
+	property = {
+			"cron.expression=0 0/3 * 1/1 * ? *" //scheduler executed every 3 mins
+			},
+	service = ScheduleCronPortlet.class
+)
+public class ScheduleCronPortlet extends BaseMessageListener {
+	
+    /*private static Log _logger = LogFactoryUtil.getLog(ScheduleCronPortlet.class);*/
+    /*private static Logger _logger = LogManager.getLogger(ScheduleCronPortlet.class.getName());*/
+    private static Logger _logger = LoggerFactory.getLogger(ScheduleCronPortlet.class.getName());
+    
+	@Override
+	protected void doReceive(Message message) throws Exception {
+		_logger.info("....Scheduled task executed...");
+		//String token = "IGQVJXZAW5iRENWTkk5N2R4ZA1Q1U1BXUk01djhZARmw4UXBtSnRIaFI4ZA2tnZAUxLLTdsa0NvZA2kzVkNHbmh6cFU3OGExMnpNV1NXZAWNFNmxuNVpocTZAwczRDTWV5WUItcU55N1NYZAkVFRG1oZAEJLLUNNMwZDZD";
+		String token = "";
+		String fields = "id,media_type,media_url,thumbnail_url,timestamp,permalink,caption";
+		//String uri = "https://graph.instagram.com/me/media?fields="+ fields +"&access_token="+ token +"&count=-1";
+		
+		List<HandleAPIKeys> handleAPIKeys = new  ArrayList<HandleAPIKeys>();
+		handleAPIKeys = HandleAPIKeysLocalServiceUtil.getHandleAPIKeyses(-1, -1);
+		if(handleAPIKeys.size()!=0) {
+			for(HandleAPIKeys apiKeys : handleAPIKeys) {
+				token = apiKeys.getApiKey();
+				String uri = "https://graph.instagram.com/me/media?fields="+ fields +"&access_token="+ token +"&count=-1";
+				_logger.info("uri.. : "+uri);
+				URL url = new URL(uri);
+				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+				connection.setRequestMethod("GET");
+				_logger.info("connection : "+connection);
+				int responseCode = connection.getResponseCode();
+				_logger.info("responseCode : "+responseCode);
+				if(responseCode==200) {
+					_logger.info("For "+apiKeys.getPageKey()+" Token Valid");
+				}else {
+					_logger.info("For "+apiKeys.getPageKey()+" Token InValid");
+					
+					String subject = "For "+apiKeys.getPageKey()+" : Token Has been Expired.";
+					String body = "<b>Hi Team</b><br><br><span><b>For "+apiKeys.getPageKey()+" : Instagram Token Has been Expired.</b></span>";
+					
+					InternetAddress fromAddress = new InternetAddress(PropsUtil.get("insta.mail.from.address"));
+					//InternetAddress fromAddress = new InternetAddress("samaresh.barui@zmail.ril.com");
+					InternetAddress toAddress = new InternetAddress(PropsUtil.get("insta.mail.to.address"));
+					//InternetAddress toAddress = new InternetAddress("jioworldcenter.test@servicesnext.com");
+					InternetAddress ccAddress = new InternetAddress(PropsUtil.get("insta.mail.cc.address"));
+					_logger.info("toAddress : "+toAddress.toString());
+					_logger.info("fromAddress : "+fromAddress.toString());
+					_logger.info("ccAddress : "+ccAddress.toString());
+					MailMessage mailMessage = new MailMessage();
+					mailMessage.setFrom(fromAddress);
+					mailMessage.setTo(toAddress);
+					//mailMessage.setCC(ccAddress);
+					mailMessage.setSubject(subject);
+					mailMessage.setBody(body);
+					mailMessage.setHTMLFormat(true);
+					
+					MailServiceUtil.sendEmail(mailMessage);
+					_logger.info("....Send Mail Succeessfully..........");
+				}
+			}
+		}
+		
+	}
+	
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) throws SchedulerException {
+
+		try {
+	    	_logger.info("__Scheduler activated__");
+	        //String cronExpression = GetterUtil.getString(properties.get("cron.expression"), "cronExpression");
+	    	String cronExpression = PropsUtil.get("cron.expression.insta");
+	        _logger.info(" cronExpression: " + cronExpression);
+	 
+	        String listenerClass = getClass().getName();
+	        Trigger jobTrigger = TriggerFactoryUtil.createTrigger(listenerClass, listenerClass, new Date(), null, cronExpression);
+	 
+	        SchedulerEntryImpl schedulerEntryImpl = new SchedulerEntryImpl(listenerClass, jobTrigger);
+	        
+	        SchedulerEngineHelperUtil.register(this, schedulerEntryImpl, DestinationNames.SCHEDULER_DISPATCH);
+	 
+	    } catch (Exception e) {
+	    	_logger.error("--- Issue in Scheduler ----");
+	    	e.printStackTrace();
+	    }
+
+	}
+	
+	@Deactivate
+	protected void deactive() {
+		_logger.info("__ Scheduler deactivated__");
+	    SchedulerEngineHelperUtil.unregister(this);
+	}
+	
+}
